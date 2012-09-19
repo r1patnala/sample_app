16230a668b82358a5077fd69d863ab507b4f49ad
Ravi Patnala <rpatnala@cisco.com>
2012-09-19 14:24:27 -0700

    adding another test case about

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,4 +14,11 @@ describe "StaticPages" do
       page.should have_content('help')
     end
   end
+  
+  describe "About page" do
+    it "should have the content 'About us'" do
+      visit "/static_pages/about"
+      page.should have_content("About Us")
+    end
+  end
 end
